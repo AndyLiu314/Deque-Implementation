fefa79b0aee033616a5947bb545926a00248cce4
Course Test <course-test@cs.sfu.ca>
2022-09-28 19:27:37 +0000

    hope

--- Assignment 1/Deque/DQtest.cpp
+++ Assignment 1/Deque/DQtest.cpp
@@ -2,19 +2,13 @@
 #include <iostream>
 using namespace std;
 
+bool test_reserve(){
+    Deque<int> d;
+
+}
 
 int main( )
 {
-    //cout<<"this works"<<endl;
-    //cout<<"this works"<<endl;
-    
-    /*
-    int k =0;
-    for (k=k; k<5;k++){
-        cout<<"this works"<<endl;
-        cout<<k<<endl;
-    }
-    cout<<k<<endl;*/
 
     Deque<int> d;
     int i = 6;
